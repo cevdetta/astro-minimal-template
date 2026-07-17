9dae3fb548990f9d0cbb22559dced8bd243cff04
Cevdet <76725496+cevdetta@users.noreply.github.com>
2026-07-17 20:01:13 +0200

    seo: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-minimal-template.pages.dev'
+  site: 'https://astro-minimal-template.pages.dev',
+  integrations: [sitemap()]
 });
